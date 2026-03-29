35be1eaf4bbd8a55969b324d648f341ae55e95f4
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-30 02:14:13 +0700

    W10-03 – Create Fetch artist songs, Fetch artist comments and Post a new comment method in artist repository

--- data/repositories/artist/artist_repository.dart
+++ data/repositories/artist/artist_repository.dart
@@ -1,3 +1,6 @@
+import 'package:w10/model/comment/comment.dart';
+import 'package:w10/model/songs/song.dart';
+
 import '../../../model/artist/artist.dart';
  
 
@@ -5,4 +8,10 @@ abstract class ArtistRepository {
   Future<List<Artist>> fetchArtists({bool forceFetch = false});
   
   Future<Artist?> fetchArtistById(String id);
+
+  Future<List<Song>> getSongsByArtist(String artistId);
+
+  Future<List<Comment>> getComments(String artistId);
+
+  Future<void> addComment(String artistId, String text);
 }
