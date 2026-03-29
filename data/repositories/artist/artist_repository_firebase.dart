265f0d1c5c9cfe72e6fbcca67c6a50929eb07d22
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-30 01:12:13 +0700

    W10-02 – Add in-memory cache in artist repository firebase

--- data/repositories/artist/artist_repository_firebase.dart
+++ data/repositories/artist/artist_repository_firebase.dart
@@ -11,9 +11,14 @@ class ArtistRepositoryFirebase implements ArtistRepository {
     'test-a2a77-default-rtdb.asia-southeast1.firebasedatabase.app',
     '/artists.json',
   );
+  List<Artist>? _cachedArtists;
 
   @override
-  Future<List<Artist>> fetchArtists() async {
+  Future<List<Artist>> fetchArtists({bool forceFetch = false}) async {
+    if (!forceFetch && _cachedArtists != null) {
+      return _cachedArtists!;
+    }
+
     final http.Response response = await http.get(artistsUri);
 
     if (response.statusCode == 200) {
@@ -24,6 +29,7 @@ class ArtistRepositoryFirebase implements ArtistRepository {
       for (final entry in songJson.entries) {
         result.add(ArtistDto.fromJson(entry.key, entry.value));
       }
+      _cachedArtists = result;
       return result;
     } else {
       // 2- Throw expcetion if any issue
