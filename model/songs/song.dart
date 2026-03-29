4bb68b882a6b622f27920e277b98cd5e5b7da22c
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-29 20:20:14 +0700

    W10-01 – Add like to Song model

--- model/songs/song.dart
+++ model/songs/song.dart
@@ -4,6 +4,7 @@ class Song {
   final String artistId;
   final Duration duration;
   final Uri imageUrl;
+  final int like;
 
   Song({
     required this.id,
@@ -11,6 +12,7 @@ class Song {
     required this.artistId,
     required this.duration,
     required this.imageUrl,
+    required this.like,
   });
 
   @override
@@ -18,3 +20,5 @@ class Song {
     return 'Song(id: $id, title: $title, artist id: $artistId, duration: $duration)';
   }
 }
+
+
